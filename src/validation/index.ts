95e9ccfc21b54576978f899b01a9dde1721bcfeb
IamLena <helly.luchina@gmail.com>
2025-09-13 13:28:01 +0300

    validation

--- src/validation/index.ts
+++ src/validation/index.ts
@@ -3,45 +3,36 @@
 
 export function loginValidation(value: string) {
     console.log('loginValidation', value);
-    if (!value) { return null }
-    if (value.length < 3 || value.length > 20) { return null }
-    if (/[\s\W]/.test(value.replace(/[-_]/g, ''))) { return null }
-    if (/^\d+$/.test(value)) { return null };
-    
-    return value;
+    const isValid = /^(?=.*[a-zA-Z])[a-zA-Z0-9-_]{3,20}$/.test(value);
+    return isValid ? value : null;
 };
 
 // password - от 8 до 40 символов, обязательно хотя бы одна заглавная буква и цифра.
 export function passwordValidation(value: string) {
     console.log('passwordValidation', value);
-    if (!value) { return null }
-    if (value.length < 8 || value.length > 40) { return null }
-    
-    return value;
+    const isValid = /^(?=.*[A-Z])(?=.*\d).{8,40}$/.test(value);
+    return isValid ? value : null;
 }
 
 // first_name, second_name — латиница или кириллица, первая буква должна быть заглавной, без пробелов и без цифр, нет спецсимволов (допустим только дефис).
 export function nameValidation(value: string) {
     console.log('nameValidation', value);
-    if (!value) { return null }
-    
-    return value;
+    const isValid = /^[A-Z][a-z-]*$/.test(value) || /^[А-ЯЁ][а-яё-]*$/.test(value);
+    return isValid ? value : null;
 }
 
 // email — латиница, может включать цифры и спецсимволы вроде дефиса и подчёркивания, обязательно должна быть «собака» (@) и точка после неё, но перед точкой обязательно должны быть буквы.
 export function emailValidation(value: string) {
     console.log('emailValidation', value);
-    if (!value) { return null }
-    
-    return value;
+    const isValid = /^[0-9a-zA-Z\-_]+@[a-zA-Z]+\.[0-9a-zA-Z\-_]+$/.test(value);
+    return isValid ? value : null;
 }
 
 // phone — от 10 до 15 символов, состоит из цифр, может начинается с плюса.
 export function phoneValidation(value: string) {
     console.log('phoneValidation', value);
-    if (!value) { return null }
-    if (value.length < 10 || value.length > 15) { return null }
-    return value;
+    const isValid = /^(?=.{10,15}$)\+?\d+$/.test(value);
+    return isValid ? value : null;
 }
 
 //message — не должно быть пустым.
--- src/validation/test.ts
+++ src/validation/test.ts
@@ -7,7 +7,7 @@ import {
     messageValidation,
 } from './index.ts';
 
-function test(testName: string, result: string, expected: string) {
+function test(testName: string, result: string | null, expected: string | null) {
     console.log(`${testName}: ${result === expected ? 'OK' : 'FAIL'}`)
 }
 
@@ -31,48 +31,50 @@ test('содержит нижнее подчеркивание', loginValidation
 // password - от 8 до 40 символов, обязательно хотя бы одна заглавная буква и цифра.
 console.log('--------PASSWORD---------');
 test('пустая строка', passwordValidation(''), null);
-test('меньше 8 символов', passwordValidation(''), null);
-test('больше 40 символов', passwordValidation(''), null);
-test('нет заглавной буквы', passwordValidation(''), null);
-test('нет цифры', passwordValidation(''), null);
+test('меньше 8 символов', passwordValidation('abc'), null);
+test('больше 40 символов', passwordValidation('oooooooooooooooooooooooooooooooooooooooo'), null);
+test('нет заглавной буквы', passwordValidation('qwertyqwerty'), null);
+test('нет цифры', passwordValidation('qwertyqwerty'), null);
 
-test('8 символов', passwordValidation(''), null);
-test('40 символов', passwordValidation(''), null);
-test('есть заглавная буква и цифра', passwordValidation(''), null);
+test('8 символов', passwordValidation('12345A78'), '12345A78');
+test('40 символов', passwordValidation('ooooooooooooooAooooooooooooooo1oooooooo'), 'ooooooooooooooAooooooooooooooo1oooooooo');
+test('есть заглавная буква и цифра', passwordValidation('sdf1234WER5678fghj'), 'sdf1234WER5678fghj');
 
 
 // first_name, second_name — латиница или кириллица, первая буква должна быть заглавной, без пробелов и без цифр, нет спецсимволов (допустим только дефис).
 console.log('--------FIRST/SECOND NAME---------');
-test('пустая строка', nameValidation(''), '');
-test('первая буква не заглавная', nameValidation(''), '');
-test('содержит цифру', nameValidation(''), '');
-test('сожержит пробел', nameValidation(''), '');
-test('содержит спецсимвол', nameValidation(''), '');
-test('состоит из латиницы и кирилицы сразу', nameValidation(''), '');
+test('пустая строка', nameValidation(''), null);
+test('первая буква не заглавная', nameValidation('bob'), null);
+test('содержит цифру', nameValidation('Bob1'), null);
+test('сожержит пробел', nameValidation('Bob bob'), null);
+test('содержит спецсимвол', nameValidation('Bob!'), null);
+test('состоит из латиницы и кирилицы сразу', nameValidation('Bobбоб'), null);
 
-test('содержит дефис', nameValidation(''), '');
-test('состоит из кирилицы', nameValidation(''), '');
-test('состоит из латиницы', nameValidation(''), '');
+test('содержит дефис', nameValidation('Bob-bob'), 'Bob-bob');
+test('состоит из кирилицы', nameValidation('Боб'), 'Боб');
+test('состоит из латиницы', nameValidation('Bob'), 'Bob');
 
 // email — латиница, может включать цифры и спецсимволы вроде дефиса и подчёркивания, обязательно должна быть «собака» (@) и точка после неё, но перед точкой обязательно должны быть буквы.
 console.log('--------EMAIL---------');
-test('', emailValidation(''), '');
+test('пустая строка', emailValidation(''), null);
+
+test('валидный', emailValidation('a@mail.ru'), 'a@mail.ru');
 
 // phone — от 10 до 15 символов, состоит из цифр, может начинается с плюса.
 console.log('--------PHONE---------');
-test('пустая строка', phoneValidation(''), '');
-test('меньше 10 символов', phoneValidation(''), '');
-test('больше 15 символов', phoneValidation(''), '');
-test('содержит символ отличный от плюса или цифры', phoneValidation(''), '');
-test('начинается с двух плючов', phoneValidation(''), '');
+test('пустая строка', phoneValidation(''), null);
+test('меньше 10 символов', phoneValidation('123456789'), null);
+test('больше 15 символов', phoneValidation('12345678901234567890'), null);
+test('содержит символ отличный от плюса или цифры', phoneValidation('12345-678a90'), null);
+test('начинается с двух плючов', phoneValidation('++1234567890'), null);
 
-test('начинается с одного плюса', phoneValidation(''), '');
-test('содержит только цифры', phoneValidation(''), '');
-test('10 символов', phoneValidation(''), '');
-test('15 символов', phoneValidation(''), '');
+test('начинается с одного плюса', phoneValidation('+1234567890'), '+1234567890');
+test('содержит только цифры', phoneValidation('123456789012'), '123456789012');
+test('10 символов', phoneValidation('1234567890'), '1234567890');
+test('15 символов', phoneValidation('123456789012345'), '123456789012345');
 
 //message — не должно быть пустым.
 console.log('--------MESSAGE---------');
-test('пустая строка', messageValidation(''), '');
-test('не пустая строка', messageValidation(''), '');
+test('пустая строка', messageValidation(''), null);
+test('не пустая строка', messageValidation('text'), 'text');
 
